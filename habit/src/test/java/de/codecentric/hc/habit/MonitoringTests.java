c5bb8835bca637a6f533029a078a54d88dd4a2a8
Michael Follmann <michael.follmann@codecentric.de>
2019-01-02 08:32:22 +0100

    [#4] integration tests for health check added

---
+++ habit/src/test/java/de/codecentric/hc/habit/MonitoringTests.java
@@ -0,0 +1,26 @@
+package de.codecentric.hc.habit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class MonitoringTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void healthCheck() {
+        given().port(this.port)
+                .when().get("/actuator/health")
+                .then().statusCode(200).body("status", equalTo("UP"));
+    }
+}
