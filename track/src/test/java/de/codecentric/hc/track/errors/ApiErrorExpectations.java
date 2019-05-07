16e9de98cacb0e7d98157bd8bebdc36464ccc3f3
Dennis Effing <dennis.effing@codecentric.de>
2019-05-07 11:44:21 +0200

    [#20] Implement endpoints which utilize user ID JWT extraction

---
+++ track/src/test/java/de/codecentric/hc/track/errors/ApiErrorExpectations.java
@@ -0,0 +1,14 @@
+package de.codecentric.hc.track.errors;
+
+public class ApiErrorExpectations {
+
+    public static final ApiError EXPECTED_USER_ID_VIOLATION = ApiError.builder()
+                                                                      .code("ConstraintViolation.UserId")
+                                                                      .detail("must not be blank and size must be between 5 and 64")
+                                                                      .build();
+
+    public static final ApiError EXPECTED_HABIT_ID_VIOLATION = ApiError.builder()
+                                                                       .code("ConstraintViolation.HabitId")
+                                                                       .detail("must be greater than 0")
+                                                                       .build();
+}
