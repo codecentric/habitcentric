d92b46d00e2f25eb60c0bed3b60f0ef51e95fd0b
Dennis Effing <dennis.effing@codecentric.de>
2019-05-07 11:39:45 +0200

    [#20] Implement mechanism for user ID extraction from a supplied JWT
    - Add an implementation of Spring's JwtDecoder interface to decode
    JWTs without verifying their signature.
    - Add an implementation of Spring's HandleMethodArgumentResolver to
    intercept HTTP header "Authorization", extract and decode the JWT and
    extract the user ID from the JWT's claims.

--- track/src/test/java/de/codecentric/hc/track/habits/HabitTrackingControllerWebMvcTest.java
+++ track/src/test/java/de/codecentric/hc/track/habits/HabitTrackingControllerWebMvcTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -44,6 +45,9 @@ public class HabitTrackingControllerWebMvcTest {
 
     @MockBean
     private HabitTrackingRepository repository;
+
+    @MockBean
+    private JwtDecoder jwtDecoder;
 
     @Test
     public void shouldReturnTrackRecords() throws Exception {
