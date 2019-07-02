a9aedc20f53798fbe56f2126db373c4b98fa410e
Michael Follmann <michael.follmann@codecentric.de>
2019-07-02 23:10:57 +0200

    [#5] UserIdFilters refactored to retrieve user ID from SecurityContext because it is more robust than extracting it from the basic authorization header

--- gateway/src/main/java/de/codecentric/hc/gateway/filters/UserIdFilters.java
+++ gateway/src/main/java/de/codecentric/hc/gateway/filters/UserIdFilters.java
@@ -1,64 +1,69 @@
 package de.codecentric.hc.gateway.filters;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.util.Base64Utils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.util.UriUtils;
+import reactor.core.publisher.Mono;
 
 public final class UserIdFilters {
 
   public static GatewayFilter REWRITE_TRACK_PATH_WITH_USER_ID =
-      (exchange, chain) -> {
-        Optional<String> userId = extractUserId(exchange.getRequest());
-        String replacement =
-            String.format(
-                "/track/users/%s/",
-                UriUtils.encodePath(
-                    userId.orElseThrow(() -> new UserIdMissingException()),
-                    StandardCharsets.UTF_8.toString()));
-        ServerHttpRequest request =
-            exchange
-                .getRequest()
-                .mutate()
-                .path(exchange.getRequest().getPath().value().replaceFirst("/track/", replacement))
-                .build();
-        return chain.filter(exchange.mutate().request(request).build());
-      };
+      (exchange, chain) ->
+          userId()
+              .flatMap(userId -> chain.filter(mutateTrackRequestWithUserIdPath(exchange, userId)));
 
-  public static GatewayFilter SET_USER_ID_HEADER =
-      (exchange, chain) -> {
-        Optional<String> userId = extractUserId(exchange.getRequest());
-        if (!userId.isPresent()) {
-          return chain.filter(exchange);
-        }
-        ServerHttpRequest request =
-            exchange
-                .getRequest()
-                .mutate()
-                .header("X-User-ID", userId.get())
-                .headers(httpHeaders -> httpHeaders.remove("Authorization"))
-                .build();
-        return chain.filter(exchange.mutate().request(request).build());
-      };
-
-  private static Optional<String> extractUserId(ServerHttpRequest request) {
-    String auth = request.getHeaders().getFirst("Authorization");
-    if (StringUtils.isEmpty(auth)) {
-      return Optional.empty();
+  private static ServerWebExchange mutateTrackRequestWithUserIdPath(
+      ServerWebExchange exchange, String userId) {
+    if (StringUtils.isEmpty(userId)) {
+      throw new UserIdMissingException();
     }
-    String base64Credentials = auth.replace("Basic ", "");
-    return extractUserId(new String(Base64Utils.decodeFromString(base64Credentials)));
+    ServerHttpRequest request =
+        exchange
+            .getRequest()
+            .mutate()
+            .path(
+                exchange
+                    .getRequest()
+                    .getPath()
+                    .value()
+                    .replaceFirst("/track/", trackPathWithUserId(userId)))
+            .build();
+    return exchange.mutate().request(request).build();
   }
 
-  private static final Pattern plainCredentialsPattern = Pattern.compile("(.*):(.*)");
+  private static String trackPathWithUserId(String userId) {
+    return String.format(
+        "/track/users/%s/", UriUtils.encodePath(userId, StandardCharsets.UTF_8.toString()));
+  }
 
-  private static Optional<String> extractUserId(String plainCredentials) {
-    Matcher matcher = plainCredentialsPattern.matcher(plainCredentials);
-    return matcher.find() ? Optional.of(matcher.group(1)) : Optional.empty();
+  public static GatewayFilter SET_USER_ID_HEADER =
+      (exchange, chain) ->
+          userId().flatMap(userId -> chain.filter(mutateRequestWithUserIdHeader(exchange, userId)));
+
+  private static ServerWebExchange mutateRequestWithUserIdHeader(
+      ServerWebExchange exchange, String userId) {
+    if (StringUtils.isEmpty(userId)) {
+      return exchange;
+    }
+    ServerHttpRequest request =
+        exchange
+            .getRequest()
+            .mutate()
+            .header("X-User-ID", userId)
+            .headers(httpHeaders -> httpHeaders.remove("Authorization"))
+            .build();
+    return exchange.mutate().request(request).build();
+  }
+
+  private static Mono<String> userId() {
+    return ReactiveSecurityContextHolder.getContext()
+        .map(SecurityContext::getAuthentication)
+        .map(Authentication::getName);
   }
 }
