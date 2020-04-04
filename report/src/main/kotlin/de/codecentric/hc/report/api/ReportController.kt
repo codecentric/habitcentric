15cc81e2eaee4c90dec963855fa2a6c364ee26bc
Dennis Effing <dennis.effing@codecentric.de>
2020-04-04 20:19:02 +0200

    Simplify Open API specification

--- report/src/main/kotlin/de/codecentric/hc/report/api/ReportController.kt
+++ report/src/main/kotlin/de/codecentric/hc/report/api/ReportController.kt
@@ -1,15 +1,14 @@
 package de.codecentric.hc.report.api
 
-import de.codecentric.hc.report.api.model.Report
-import de.codecentric.hc.report.api.model.Stats
+import de.codecentric.hc.report.api.model.AchievementRate
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ReportController : ReportsApi {
 
-    override fun reportGet(): ResponseEntity<Report> {
-        val report = Report(Stats(42), Stats(42), Stats(42), Stats(42))
-        return ResponseEntity.ok(report)
+    override fun reportAchievementGet(): ResponseEntity<AchievementRate> {
+        val achievementRate = AchievementRate(42f, 42f)
+        return ResponseEntity.ok(achievementRate)
     }
 }
