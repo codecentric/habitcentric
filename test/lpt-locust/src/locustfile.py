7fa0d6941fcf96aeadc72e813373bfa51907fd36
Dennis Effing <dennis.effing@codecentric.de>
2020-09-04 11:42:28 +0200

    Generate habit with track records and clean up afterwards

--- test/lpt-locust/src/locustfile.py
+++ test/lpt-locust/src/locustfile.py
@@ -8,5 +8,20 @@ class HabitcentricUser(OidcHttpUser):
 
     @task
     def index_page(self):
+        habit = {
+            'name': 'Test',
+            'schedule': {
+                'repetitions': 1,
+                'frequency': 'DAILY'
+            }
+        }
+        create_response = self.client.post(url='/habits', json=habit, verify=False)
+        created_habit_id = create_response.headers['location'].split('/')[-1]
+
+        self.client.put('/track/habits/{}'.format(created_habit_id), name='/track/habits/[id]', json=['2020-09-10', '2020-09-09', '2020-09-08'])
+
         self.client.get(url='/habits', verify=False)
         self.client.get(url='/report/achievement', verify=False)
+
+        self.client.put('/track/habits/{}'.format(created_habit_id), name='/track/habits/[id]', json=[])
+        self.client.delete('/habits/{}'.format(created_habit_id), name='/habits/[id]')
