1518eed60846ea98c7d64a695fa46acac240b622
Dennis Effing <dennis.effing@codecentric.de>
2020-11-13 16:29:19 +0100

    Add proof-of-concept implementation for manual trace header forwarding
    The issue with utilizing Spring Cloud Sleuth's header propagation is
    that it also creates new spans. In service mesh environments, we do not
    want Sleuth to create new spans, because the mesh is doing that for us
    already.

--- report/src/main/kotlin/de/codecentric/hc/report/Configuration.kt
+++ report/src/main/kotlin/de/codecentric/hc/report/Configuration.kt
@@ -22,17 +22,55 @@ class Configuration {
         if (authHeader != null && authHeader.isNotEmpty()) {
             restTemplateBuilder = addJwtInterceptor(restTemplateBuilder, authHeader)
         }
+
+        val requestIdHeader = incomingRequest.getHeader("x-request-id")
+        val traceIdHeader = incomingRequest.getHeader("x-b3-traceid")
+        val spanIdHeader = incomingRequest.getHeader("x-b3-spanid")
+        val parentSpanIdHeader = incomingRequest.getHeader("x-b3-parentspanid")
+        val sampledHeader = incomingRequest.getHeader("x-b3-sampled")
+        val flagsHeader = incomingRequest.getHeader("x-b3-flags")
+        val spanContextHeader = incomingRequest.getHeader("x-ot-span-context")
+
+        val map = mapOf(
+                "x-request-id" to requestIdHeader,
+                "x-b3-traceid" to traceIdHeader,
+                "x-b3-spanid" to spanIdHeader,
+                "x-b3-parentspanid" to parentSpanIdHeader,
+                "x-b3-sampled" to sampledHeader,
+                "x-b3-flags" to flagsHeader,
+                "x-ot-span-context" to spanContextHeader
+        )
+
+        restTemplateBuilder = addTraceInterceptor(restTemplateBuilder, map)
+
         return restTemplateBuilder.build()
     }
 
     private fun addJwtInterceptor(
-        restTemplateBuilder: RestTemplateBuilder, authHeader: String
+            restTemplateBuilder: RestTemplateBuilder, authHeader: String
     ): RestTemplateBuilder {
         return restTemplateBuilder.additionalInterceptors(
-            ClientHttpRequestInterceptor { outgoingRequest: HttpRequest, body: ByteArray?, execution: ClientHttpRequestExecution ->
-                outgoingRequest.headers[HttpHeaders.AUTHORIZATION] = authHeader
-                execution.execute(outgoingRequest, body!!)
-            }
+                ClientHttpRequestInterceptor { outgoingRequest: HttpRequest, body: ByteArray?, execution: ClientHttpRequestExecution ->
+                    outgoingRequest.headers[HttpHeaders.AUTHORIZATION] = authHeader
+                    execution.execute(outgoingRequest, body!!)
+                }
+        )
+    }
+
+    private fun addTraceInterceptor(
+            restTemplateBuilder: RestTemplateBuilder, traceHeaders: Map<String, String>
+    ): RestTemplateBuilder {
+        return restTemplateBuilder.additionalInterceptors(
+                ClientHttpRequestInterceptor { outgoingRequest: HttpRequest, body: ByteArray?, execution: ClientHttpRequestExecution ->
+                    outgoingRequest.headers["x-request-id"] = traceHeaders.get("x-request-id")
+                    outgoingRequest.headers["x-b3-traceid"] = traceHeaders.get("x-b3-traceid")
+                    outgoingRequest.headers["x-b3-spanid"] = traceHeaders.get("x-b3-spanid")
+                    outgoingRequest.headers["x-b3-parentspanid"] = traceHeaders.get("x-b3-parentspanid")
+                    outgoingRequest.headers["x-b3-sampled"] = traceHeaders.get("x-b3-sampled")
+                    outgoingRequest.headers["x-b3-flags"] = traceHeaders.get("x-b3-flags")
+                    outgoingRequest.headers["x-ot-span-context"] = traceHeaders.get("x-ot-span-context")
+                    execution.execute(outgoingRequest, body!!)
+                }
         )
     }
 }
