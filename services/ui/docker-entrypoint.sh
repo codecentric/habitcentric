4ad2fcf5c17b0ad98314d0115d222ec1d8b4898e
Dennis Effing <dennis.effing@codecentric.de>
2022-05-20 13:54:44 +0200

    chore(ui): remove in favor of new react UI

--- services/ui/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-appFile="$(ls /usr/share/nginx/html/dist/js/app*.js)"
-envsubst '$HABIT_HOST,$TRACKING_HOST,$REPORT_HOST,$OIDC_AUTH,$OIDC_AUTHORITY_HOST,$OIDC_CLIENT_ID' < ${appFile} > app.js.tmp
-mv app.js.tmp ${appFile}
-exec "$@"
--- services/ui/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-app dark>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-lowercase"> Habitcentric </v-toolbar-title>
-    </v-toolbar>
-    <v-content><router-view /></v-content>
-  </v-app>
-</template>
